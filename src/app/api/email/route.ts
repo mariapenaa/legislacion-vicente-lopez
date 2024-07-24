0c7fc4b66c59a840b4f1f9f470a229f71179e197
mariapenaa <63109876+mariapenaa@users.noreply.github.com>
2024-07-24 10:02:50 -0300

    Cambio mail

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -15,7 +15,7 @@ export async function POST(request: NextRequest) {
 
   const mailOptions: Mail.Options = {
     from: process.env.MY_EMAIL,
-    to: process.env.MY_EMAIL,
+    to: process.env.RECIEVING_EMAIL,
     // cc: email, (uncomment this line if you want to send a copy to the sender)
     subject: `Nuevo mensaje de ${name} (${email}) - Portal legislaciones`,
     text: message,
