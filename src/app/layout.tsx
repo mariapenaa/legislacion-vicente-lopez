21325bbe89202de424024334acde91b3fe8a00dc
maria <mariapenafd@gmail.com>
2025-02-05 15:23:29 -0300

    Agregue los tags de google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import theme from '../styles/theme';
 import Footer from "@/components/Footer";
 import Head from "next/head";
 import GoogleAnalytics from '@/components/GoogleAnalytics';
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,9 +35,26 @@ export default function RootLayout({
         />
         {/* <title>{metadata.title.default}</title>
         <meta name="description" content={metadata.description} /> */}
+        {/* Google Tag Manager - Head */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PLDQVGSG');`}
+        </Script>
       </Head>
       <GoogleAnalytics />
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) - Body */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PLDQVGSG"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <ThemeProvider theme={theme}>
           <Header />
           <div className="child-container">
